def3b83d341720aadaa01eedf23beda9b29eb852
cbibi <cbibi@formiris.org>
2022-02-24 10:59:44 +0100

    Pitest

---
+++ ArchiLog/WebApp.XUnit.Test/Pitest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace WebApp.XUnit.Test
+{
+
+    public class Pitest
+    {
+
+        [Fact]
+        public void TestAdd()
+        {
+            //Given
+            Calculatrice calc = new Calculatrice();
+
+            //When
+            int result = calc.Add(0, 0);
+
+            //Then
+            result.Should().Be(0);
+        }
+    }
+
+    public class Calculatrice
+    {
+        internal int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
